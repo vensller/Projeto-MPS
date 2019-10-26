1dc07db0600fb546f93cf572070a65f443ad37ec
Douglas <martins.douglas.souza@gmail.com>
2019-10-26 14:40:24 -0300

    Rotina para deletar a empresa implementada

--- ProjetoMPS/MPSPage/infostore/src/components/CompanyRegister.vue
+++ ProjetoMPS/MPSPage/infostore/src/components/CompanyRegister.vue
@@ -1,96 +1,100 @@
 <template>
-  <v-card>    
+  <v-card>
     <v-card-title>
       <span class="headline">Cadastro de empresas</span>
     </v-card-title>
     <v-card-text>
       <v-container grid-list-md>
-        <v-layout wrap>              
+        <v-layout wrap>
           <v-flex xs9>
             <v-text-field label="Nome da empresa" v-model="company" required></v-text-field>
           </v-flex>
           <v-flex xs3>
             <v-text-field label="Classificação" v-model="classification" required></v-text-field>
-          </v-flex>          
+          </v-flex>
         </v-layout>
       </v-container>
     </v-card-text>
     <v-card-actions>
       <v-spacer></v-spacer>
-      <v-btn color="blue darken-1" flat @click="submit">Salvar</v-btn>          
+      <v-btn color="blue darken-1" flat @click="submit">Salvar</v-btn>
       <v-btn color="blue darken-1" flat @click="close">Fechar</v-btn>
-    </v-card-actions>    
+    </v-card-actions>
     <v-snackbar
-        v-model="snackbar" 
-        :bottom="false"
-        :left="false"
-        :multi-line="true"
-        :right="false"
-        :timeout="6000"
-        :top="true"
-        :vertical="false">
-        {{text}}
-        <v-btn
-            color="pink"
-            flat
-            @click="snackbar = false"
-        >Close</v-btn>
+      v-model="snackbar"
+      :bottom="false"
+      :left="false"
+      :multi-line="true"
+      :right="false"
+      :timeout="6000"
+      :top="true"
+      :vertical="false"
+    >
+      {{text}}
+      <v-btn color="pink" flat @click="snackbar = false">Close</v-btn>
     </v-snackbar>
-  </v-card>     
+  </v-card>
 </template>
 
 <script>
-export default{
-    data(){
-        return{
-            company:"",
-            classification: "",
-            text: "",
-            snackbar: false,
-            registered: false
-        }    
-    },
-    methods:{        
-        submit(){                    
-          let data = JSON.stringify({
-            company: this.company,
-            classification: this.classification,
-            comments: []            
-          });          
+export default {
+  data() {
+    return {
+      company: "",
+      classification: "",
+      text: "",
+      snackbar: false,
+      registered: false
+    };
+  },
+  methods: {
+    submit() {
+      let data = JSON.stringify({
+        company: this.company,
+        classification: this.classification,
+        comments: []
+      });
 
-          var vm = this;
+      var vm = this;
 
-          fetch("http://localhost:3000/company", {            
-            headers: {
-                'accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            method: "PUT",
-            body: data
-          }).then(response => response.json()).then(data => {                
-            if (data.error){
-              vm.text = data.error;                
-              vm.snackbar = true;
-            }else{
-              vm.text = "Empresa cadastrada com sucesso!";                
-              vm.snackbar = true;  
-              vm.registered = true;                                                          
-              vm.reset();
-            }
-          }).catch(function(error) {            
-            vm.text = error;
+      fetch("http://localhost:3000/company", {
+        headers: {
+          accept: "application/json",
+          "Content-Type": "application/json"
+        },
+        method: "PUT",
+        body: data
+      })
+        .then(response => response.json())
+        .then(data => {
+          if (data.error) {
+            vm.text = data.error;
             vm.snackbar = true;
+          } else {
+            vm.text = "Empresa cadastrada com sucesso!";
+            vm.snackbar = true;
+            vm.registered = true;
             vm.reset();
-          });
-        },
-        reset(){
-          this.company = "";
-          this.classification = "";
-        },
-        close(){
-          this.reset();
-          this.$emit("OnCloseCompanyScreen", this.registered);
-        }
-    }
-}
+          }
+        })
+        .catch(function(error) {
+          vm.text = error;
+          vm.snackbar = true;
+          vm.reset();
+        });
+    },
+    reset() {
+      this.company = "";
+      this.classification = "";
+    },
+    close() {
+      this.reset();
+      this.$emit("OnCloseCompanyScreen", this.registered);
+    },
+    updateCompany(company) {}
+  },
+  created() {
+    this.$emit("onCompanyCreated", this.updateCompany);
+  }
+};
 </script>
--- ProjetoMPS/MPSPage/infostore/src/views/CompanyPage.vue
+++ ProjetoMPS/MPSPage/infostore/src/views/CompanyPage.vue
@@ -2,104 +2,122 @@
   <v-card>
     <v-card-title>
       <h1>Empresas - MPS/BR</h1>
-      
+
       <v-btn icon @click="dialog = true">
         <v-icon>add</v-icon>
       </v-btn>
 
       <v-spacer></v-spacer>
-      
-      <v-text-field
-        v-model="search"
-        append-icon="search"
-        label="Search"
-        single-line
-        hide-details
-      ></v-text-field>
+
+      <v-text-field v-model="search" append-icon="search" label="Search" single-line hide-details></v-text-field>
     </v-card-title>
-    
+
     <v-data-table
       :headers="headers"
       :items="companies"
       :search="search"
-      :pagination.sync="pagination"        
+      :pagination.sync="pagination"
       class="elevation-1"
-     >      
+    >
       <template v-slot:items="props">
-        <tr>          
+        <tr>
           <td>{{props.item.company}}</td>
           <td>{{props.item.classification}}</td>
           <td class="text-xs-center">
             <v-btn icon @click="showComments(companies.find(i => i === props.item))">
-              <v-icon> 
-                library_books
-              </v-icon>
+              <v-icon>library_books</v-icon>
             </v-btn>
-          </td>       
+            <v-btn text icon @click="deleteCompany(companies.find(i => i === props.item))">
+              <v-icon>delete</v-icon>
+            </v-btn>
+            <v-btn text icon @click="updateCompany(companies.find(i => i === props.item))">
+              <v-icon>account_circle</v-icon>
+            </v-btn>
+          </td>
         </tr>
       </template>
     </v-data-table>
     <v-dialog v-model="dialog" persistent max-width="800">
-      <company-register 
-        v-on:OnCloseCompanyScreen="closeRegister">
-      </company-register>
-    </v-dialog>    
+      <company-register
+        v-on:onCompanyCreated="componentCreated"
+        v-on:OnCloseCompanyScreen="closeRegister"
+      ></company-register>
+    </v-dialog>
   </v-card>
 </template>
 
 <script>
 import CompanyRegister from '../components/CompanyRegister'
 export default {
-    data() {
-        return {
-            pagination: {
-                rowsPerPage: 10},
-            search: '',
-            headers: [
-                {
-                    text: 'Empresa',
-                    value: 'company'
-                },
-                { 
-                    text: 'Classificação', 
-                    value: 'classification' 
-                },
-                {
-                    text: 'Opções',
-                    value: '_id',
-                    sortable: false,
-                    align: 'center'            
-                }          
-                ],
-            companies: [],
-            dialog: false,
-            dialogOrders: false,
-            editFunc: Function
-        }
-    },
-    components: {
-        'company-register': CompanyRegister
-    },
-    methods: {
-        fetchCompanies(){
-            fetch("http://localhost:3000/company").then(response => response.json()).then(data => {
-                this.companies = data;
-            }).catch(function(error){
-                console.log(error);
-            });
+  data () {
+    return {
+      pagination: {
+        rowsPerPage: 10
+      },
+      search: '',
+      headers: [
+        {
+          text: 'Empresa',
+          value: 'company'
         },
-        closeRegister(pRegistered){          
-            this.dialog = false;
-            if (pRegistered)
-              this.fetchCompanies();
+        {
+          text: 'Classificação',
+          value: 'classification'
         },
-        showComments(company){
-          localStorage.setItem("company", JSON.stringify(company));
-          this.$router.push({name: "comments"});
+        {
+          text: 'Opções',
+          value: '_id',
+          sortable: false,
+          align: 'center'
         }
-    },
-    mounted(){
-        this.fetchCompanies();
+      ],
+      companies: [],
+      dialog: false,
+      dialogOrders: false,
+      editFunc: Function
     }
+  },
+  components: {
+    'company-register': CompanyRegister
+  },
+  methods: {
+    fetchCompanies () {
+      fetch('http://localhost:3000/company')
+        .then(response => response.json())
+        .then(data => {
+          this.companies = data
+        })
+        .catch(function (error) {
+          console.log(error)
+        })
+    },
+    closeRegister (pRegistered) {
+      this.dialog = false
+      if (pRegistered) this.fetchCompanies()
+    },
+    showComments (company) {
+      localStorage.setItem('company', JSON.stringify(company))
+      this.$router.push({ name: 'comments' })
+    },
+    deleteCompany (company) {
+      console.log('Company: ', company)
+      fetch('http://localhost:3000/company/delete', {
+        method: 'DELETE',
+        body: company
+      })
+        .then(response => response.json())
+        .catch(erro => console.log(erro))
+    },
+    updateCompany (company) {
+      this.editFunc(company)
+      this.dialog = true
+    },
+    componentCreated (item) {
+      this.editFunc = item
+    }
+  },
+  mounted () {
+    this.fetchCompanies()
+  }
 }
 </script>
